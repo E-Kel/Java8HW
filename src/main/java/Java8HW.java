5f8763e31eecf67d6f7fe2028613be4a0f14a3a2
qa engineer <kotarakh@gmail.com>
2020-04-10 23:04:40 +0300

    Done

--- src/main/java/Java8HW.java
+++ src/main/java/Java8HW.java
@@ -1,18 +1,40 @@
 import org.apache.log4j.Logger;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Java8HW {
     static Logger LOGGER  = Logger.getLogger(Java8HW.class.getName());
 
     public static void main(String[] args) {
         List<String> strList=Arrays.asList("animal","aan","abn","axn");
+        List<Integer> intList = new ArrayList<Integer>();
+        for(int i = -100; i <101 ; i+=3) intList.add(i);
+        Collections.shuffle(intList);
+
+        LOGGER.info("The list"+ strList+" is filtered by elements that start with the letter 'a' (lower case)" +
+                " and have exactly 3 letters: \n " +
+                ""+filterStartWithAAndThreeLettersInString(strList));
+        LOGGER.info("The list"+ intList+" is modified with the format to \n"+
+                addCommaAndAddSymbolsToOddAndEvenPartsOfNumber(intList));
+        LOGGER.info("The amount of numbers which ON MODULE more than 50 in the list: \n"+intList+ "\n is:"+filterAbs50(intList));
 
     }
 
-
-    private static List<String> filterStartWithAAndThreeLetters(List<String> arr){
-        return arr.stream().filter()
+    private static List<String> filterStartWithAAndThreeLettersInString(List<String> arr){
+        return arr.stream()
+            .filter(str -> str.length() == 3 && str.startsWith("a"))
+                .collect(Collectors.toList());
     }
+    private static String addCommaAndAddSymbolsToOddAndEvenPartsOfNumber(List<Integer> arr){
+        return arr.stream()
+        .map(num -> (num % 2) == 0 ? "e" + num : "o" + num)
+                .collect(Collectors.joining(","));
+    }
+    private static int filterAbs50(List<Integer> arr){
+        return (int) arr.stream()
+                .filter(num -> Math.abs(num) > 50)
+                .count();
+    }
+
 }
