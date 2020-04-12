4f3e470bbb39eb4fa4727eb08f2cfc8932fe90cd
qa engineer <kotarakh@gmail.com>
2020-04-12 15:22:14 +0300

    Reformatted code

--- src/main/java/Java8HW.java
+++ src/main/java/Java8HW.java
@@ -4,34 +4,36 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Java8HW {
-    static Logger LOGGER  = Logger.getLogger(Java8HW.class.getName());
+    static Logger LOGGER = Logger.getLogger(Java8HW.class.getName());
 
     public static void main(String[] args) {
-        List<String> strList=Arrays.asList("animal","aan","abn","axn");
+        List<String> strList = Arrays.asList("animal", "aan", "abn", "axn");
         List<Integer> intList = new ArrayList<Integer>();
-        for(int i = -100; i <101 ; i+=3) intList.add(i);
+        for (int i = -100; i < 101; i += 3) intList.add(i);
         Collections.shuffle(intList);
 
-        LOGGER.info("The list"+ strList+"\n is filtered by elements that start with the letter 'a'" +
+        LOGGER.info("The list" + strList + "\n is filtered by elements that start with the letter 'a'" +
                 " and have exactly 3 letters: \n " +
-                ""+filterStartWithAAndThreeLettersInString(strList));
-        LOGGER.info("The list"+ intList+" is modified with the format to \n"+
+                "" + filterStartWithAAndThreeLettersInString(strList));
+        LOGGER.info("The list" + intList + " is modified with the format to \n" +
                 addCommaAndAddcharactersToOddAndEvenNumbers(intList));
-        LOGGER.info("The amount of numbers which ON MODULE more than 50 in the list: \n"+intList+ "\n is:"+filterAbs50(intList));
+        LOGGER.info("The amount of numbers which ON MODULE more than 50 in the list: \n" + intList + "\n is:" + filterAbs50(intList));
 
     }
 
-    private static List<String> filterStartWithAAndThreeLettersInString(List<String> arr){
+    private static List<String> filterStartWithAAndThreeLettersInString(List<String> arr) {
         return arr.stream()
-            .filter(str -> str.length() == 3 && str.startsWith("a"))
+                .filter(str -> str.length() == 3 && str.startsWith("a"))
                 .collect(Collectors.toList());
     }
-    private static String addCommaAndAddcharactersToOddAndEvenNumbers(List<Integer> arr){
+
+    private static String addCommaAndAddcharactersToOddAndEvenNumbers(List<Integer> arr) {
         return arr.stream()
-        .map(num -> (num % 2) == 0 ? "e" + num : "o" + num)
+                .map(num -> (num % 2) == 0 ? "e" + num : "o" + num)
                 .collect(Collectors.joining(","));
     }
-    private static int filterAbs50(List<Integer> arr){
+
+    private static int filterAbs50(List<Integer> arr) {
         return (int) arr.stream()
                 .filter(num -> Math.abs(num) > 50)
                 .count();
